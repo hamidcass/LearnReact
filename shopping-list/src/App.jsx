4b6441866e29cdd543def6e32ee34ba3ce5d15b3
Hamid <hcassama@uoguelph.ca>
2024-11-18 14:04:59 -0500

    finished workshop

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,8 +1,64 @@
 // Step 4. Import React and useState
+import React, {useState} from 'react';
 
 // Step 5. Import the App.css file
+import './App.css'
 
 // Step 6. Create a functional component called App
+export default function App(){
+
+    const [tasks, setTasks] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleAddTask = () => {
+        if(inputValue.trim()){
+            setTasks([...tasks, inputValue])
+            setInputValue('')
+        }
+    };
+
+    const handleDeleteTask = (index) => {
+        const newTaskList = tasks.filter((task, i) => i !== index)
+
+        setTasks(newTaskList)
+    }
+
+    return (
+        <div className='list'>
+            <h1>Shopping List</h1>
+            <div className='input'>
+                <input
+                    type='text'
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder='Enter an item'
+                />
+                <button onClick={handleAddTask}>Add</button>
+
+            </div>
+            <ul>
+                {tasks.map((task,index) => (
+                    <li key={index} className='task-item'>
+                        {task}
+                        <div>
+                            <div className='tasks-actions'>
+                                <button onClick={() => handleDeleteTask(index)}>Purchase</button>
+                            </div>
+                            
+                        </div>
+                    </li>
+                    
+                ))}
+            </ul>
+        </div>
+    );
+
+}
+
 
     // Step 8a. Initialize useState state variables
 
@@ -37,5 +93,5 @@
                 {/* Step 10a. Iterate over every task and retrieve its index*/}
                 
 
-
+    
 
